e596a7fc454e5c62263202bd55266d1ba6b1cecb
David Richard Smith <DavidRichardSmith@protonmail.com>
2020-01-25 21:45:24 -0800

    added project files.

---
+++ Formatter/Native/NativeHeaderParser.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Formatter.Native
+{
+    /// <summary>
+    /// Represents a single native.
+    /// </summary>
+    public struct Native
+    {
+        /// <summary>
+        /// The name of the native.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The hash of the native.
+        /// </summary>
+        public string Hash { get; set; }
+	}
+
+    /// <summary>
+    /// Parser responsible for extracting native data inside of a natives.h file.
+    /// </summary>
+    public class NativeHeaderParser
+    {
+        #region PUBLIC
+
+		/// <summary>
+		/// Construct <see cref="NativeHeaderParser"/> with path to natives.h file.
+		/// </summary>
+		/// <param name="fileName"></param>
+		public NativeHeaderParser(string fileName)
+        {
+            _fileName = fileName;
+            _natives  = new List<Native>();
+        }
+
+        /// <summary>
+        /// Read all header data into virtual memory.
+        /// </summary>
+        /// <param name="lineBufferSize">Pre allocated line buffer size, this should at least be the number of lines in the header file.</param>
+        public void Initialize(int lineBufferSize = 5354)
+        {
+            _fileLines = new List<string>();
+			using(var reader = File.OpenText(_fileName))
+            {
+				while(!reader.EndOfStream)
+                {
+                    _fileLines.Add(reader.ReadLine());
+                }
+            }
+        }
+
+		/// <summary>
+		/// Start parsing the header data.
+		/// </summary>
+		public void Start()
+        {
+			foreach(var line in _fileLines)
+                processLine(line);
+        }
+
+		/// <summary>
+		/// Retrieve results of the parse.
+		/// </summary>
+		/// <returns></returns>
+		public List<Native> GetResults()
+        {
+            return _natives;
+        }
+
+        /// <summary>
+        /// Retrieve the length of the longest native name.
+        /// </summary>
+        /// <returns></returns>
+        public int GetLongestNativeName()
+        {
+            return _longestNativeName;
+        }
+
+        #endregion
+
+        #region PRIVATE
+
+        /// <summary>
+        /// Process raw header file line.
+        /// </summary>
+        /// <param name="line"></param>
+		private void processLine(string line)
+        {
+            // can't process lines less then 7.
+            if (line.Length < 7)
+                return;
+
+            // if we found a line containing a native declaration.
+            if (line.Substring(0, 7) == "\tstatic")
+            {
+                var native = processDeclaration(line);
+
+                if (native.Name.Length > _longestNativeName)
+                    _longestNativeName = native.Name.Length;
+
+                _natives.Add(native);
+            }
+        }
+
+        /// <summary>
+        /// Process line containing native declaration.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private Native processDeclaration(string line)
+        {
+            Native native = new Native();
+
+            native.Name = getNativeName(line);
+            native.Hash = getNativeHash(line);
+
+            return native;
+        }
+
+		/// <summary>
+		/// Retrieve the name of the native by line content.
+		/// </summary>
+		/// <param name="line"></param>
+		/// <returns></returns>
+		private string getNativeName(string line)
+        {
+            // retrieve start of native name by by finding the first space after the native return value type.
+			int index  = line.Substring(8, line.Length - 8).IndexOf(" ") + 9;
+
+            // retrieve the length by finding the start of the function arguments.
+            int length = line.Substring(index, line.Length - index).IndexOf("(");
+
+            return line.Substring(index, length);
+        }
+
+		/// <summary>
+		/// Retrieve the hash of the native by line content.
+		/// </summary>
+		/// <param name="line"></param>
+		/// <returns></returns>
+		private string getNativeHash(string line)
+        {
+            // retrieve start of native hash by finding the ending template type mark.
+            int index  = line.IndexOf(">") + 2;
+
+            // native hashes are always 18 characters long.
+            int length = 18;
+
+            return line.Substring(index, length);
+        }
+
+        #endregion
+
+        private string       _fileName;
+        private List<string> _fileLines;
+        private List<Native> _natives;
+        private int          _longestNativeName;
+    }
+}
---
+++ Formatter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+using Formatter.Native;
+
+namespace Formatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // must provide at least two arguments.
+            if(args.Length < 2)
+            {
+                Console.WriteLine("\nsyntax error!");
+                Console.WriteLine("required arguments: string <natives_path> bool <use_padding>");
+                return;
+            }
+
+            try
+            {
+                var parser = new NativeHeaderParser(args[0]);
+
+                parser.Initialize();
+                parser.Start();
+
+                using (StreamWriter outputFile = new StreamWriter("natives_output.h"))
+                {
+                    outputFile.WriteLine("enum class eNatives : std::uint64_t");
+                    outputFile.WriteLine("{");
+
+                    bool applyPadding = Convert.ToBoolean(args[1]);
+                    foreach (var native in parser.GetResults())
+                    {
+                        // amount of padding to appened to the right side of the native name.
+                        int paddingCount = applyPadding ? GetNativeNamePadding(native.Name, parser.GetLongestNativeName()) : 0;
+
+                        // write formatted string to output file.
+                        outputFile.WriteLine("\t" + native.Name.PadRight(paddingCount) + " = " + native.Hash + ",");
+                    }
+
+                    outputFile.WriteLine("};");
+                }
+
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("failed to find input natives.h file.");
+                Console.ReadLine();
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve the amount of padding that should be applied to the right side of the native name.
+        /// </summary>
+        /// <param name="nativeName">The native name.</param>
+        /// <param name="maxPadding">The max amount of padding that should be applied.</param>
+        /// <returns></returns>
+        static int GetNativeNamePadding(string nativeName, int maxPadding)
+        {
+            // if the native name is less then the max padding we must append the max
+            // amount to keep everything in a perfect row.
+            if (nativeName.Length < maxPadding)
+                return maxPadding;
+
+            return nativeName.Length - maxPadding;
+        }
+    }
+}
